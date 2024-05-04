a7fa2851d742d62523a7d828add590178f434279
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-05-04 11:48:46 +0200

    Testing deviceOrientation on phone

--- src/Components/Prototype-Orientation/DeviceOrientationLogic.jsx
+++ src/Components/Prototype-Orientation/DeviceOrientationLogic.jsx
@@ -1,29 +1,10 @@
 import React, { useState, useEffect } from "react";
 
-const DeviceOrientationLogic = () => {
+const DeviceOrientationLogics = () => {
   const [orientationData, setOrientationData] = useState(null);
   const [motionData, setMotionData] = useState(null);
 
   useEffect(() => {
-    const orientationHandler = (event) => {
-      const alpha = event.alpha;
-      const beta = event.beta;
-      const gamma = event.gamma;
-      setOrientationData({ alpha, beta, gamma });
-    };
-
-    const motionHandler = (event) => {
-      const acceleration = event.acceleration;
-      const accelerationIncludingGravity = event.accelerationIncludingGravity;
-      const rotationRate = event.rotationRate;
-
-      setMotionData({
-        acceleration,
-        accelerationIncludingGravity,
-        rotationRate,
-      });
-    };
-
     const requestPermission = () => {
       if (
         typeof DeviceOrientationEvent !== "undefined" &&
@@ -32,7 +13,9 @@ const DeviceOrientationLogic = () => {
         DeviceOrientationEvent.requestPermission()
           .then((response) => {
             if (response === "granted") {
-              window.addEventListener("devicemotion", motionHandler);
+              window.addEventListener("devicemotion", (e) => {
+                // Handle 'e' here (e.g., update UI based on motion data)
+              });
             }
           })
           .catch(console.error);
@@ -40,40 +23,71 @@ const DeviceOrientationLogic = () => {
         alert("DeviceMotionEvent is not defined");
       }
     };
+    const btn = document.getElementById("request");
+    btn.addEventListener("click", requestPermission);
 
-    window.addEventListener("deviceorientation", orientationHandler, true);
+    const handleDeviceOrientation = (event) => {
+      const alpha = event.alpha; // rotation around the z-axis
+      const beta = event.beta; // rotation around the x-axis
+      const gamma = event.gamma; // rotation around the y-axis
+      setOrientationData({ alpha, beta, gamma });
+
+      // Use the orientation data as needed
+    };
+
+    const handleDeviceMotion = (event) => {
+      const acceleration = event.acceleration; // Acceleration data
+      const accelerationIncludingGravity = event.accelerationIncludingGravity; // Acceleration data including gravity
+      const rotationRate = event.rotationRate; // Device rotation rate
+
+      // Use the motion data as needed
+      setMotionData({
+        acceleration,
+        accelerationIncludingGravity,
+        rotationRate,
+      });
+    };
+
+    window.addEventListener("deviceorientation", handleDeviceOrientation, true);
+    window.addEventListener("devicemotion", handleDeviceMotion, true);
+
     requestPermission();
 
     return () => {
-      window.removeEventListener("deviceorientation", orientationHandler);
-      window.removeEventListener("devicemotion", motionHandler);
+      window.removeEventListener("deviceorientation", handleDeviceOrientation);
+      window.removeEventListener("devicemotion", handleDeviceMotion);
     };
   }, []);
 
   return (
     <div>
-      <h2>Orientation Data</h2>
-      {orientationData && (
-        <ul>
-          <li>Alpha: {orientationData.alpha}</li>
-          <li>Beta: {orientationData.beta}</li>
-          <li>Gamma: {orientationData.gamma}</li>
-        </ul>
-      )}
-      <h2>Motion Data</h2>
-      {motionData && (
-        <ul>
-          <li>Acceleration: {JSON.stringify(motionData.acceleration)}</li>
-          <li>
-            Acceleration Including Gravity:
-            {JSON.stringify(motionData.accelerationIncludingGravity)}
-          </li>
-          <li>Rotation Rate: {JSON.stringify(motionData.rotationRate)}</li>
-        </ul>
-      )}
-      <button id="request">Request Permission</button>
+      <h1>Device Orientation & Motion Demo 😲</h1>
+      <div>
+        <h2>Orientation Data</h2>
+        {orientationData && (
+          <ul>
+            <li>Alpha: {orientationData.alpha}</li>
+            <li>Beta: {orientationData.beta}</li>
+            <li>Gamma: {orientationData.gamma}</li>
+          </ul>
+        )}
+      </div>
+      <div>
+        <h2>Motion Data</h2>
+        {motionData && (
+          <ul>
+            <li>Acceleration: {JSON.stringify(motionData.acceleration)}</li>
+            <li>
+              Acceleration Including Gravity:{" "}
+              {JSON.stringify(motionData.accelerationIncludingGravity)}
+            </li>
+            <li>Rotation Rate: {JSON.stringify(motionData.rotationRate)}</li>
+          </ul>
+        )}
+        <button id="request">Request Permission</button>
+      </div>
     </div>
   );
 };
 
-export default DeviceOrientationLogic;
+export default DeviceOrientationLogics;
--- src/Components/Prototype-Orientation/SphereScene.jsx
+++ src/Components/Prototype-Orientation/SphereScene.jsx
@@ -1,96 +1,130 @@
-// ThreeScene.js
-import React, { useRef, useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import * as THREE from "three";
+import { StereoEffect } from "three/examples/jsm/effects/StereoEffect.js";
 
-const SphereScene = ({ orientationData }) => {
-  const canvasRef = useRef(null);
+const SphereScene = () => {
+  const containerRef = useRef(null);
+  const mouseX = useRef(0);
+  const mouseY = useRef(0);
   const spheres = useRef([]);
-  const camera = useRef(null);
+  const cameraRef = useRef(null);
 
   useEffect(() => {
-    if (!canvasRef.current) return;
+    let camera, scene, renderer, effect, directionalLight;
 
-    const scene = new THREE.Scene();
+    const init = () => {
+      const container = containerRef.current;
+      // Creating a perspective camera
+      camera = new THREE.PerspectiveCamera(
+        60,
+        window.innerWidth / window.innerHeight,
+        1,
+        100000
+      );
+      camera.position.z = 3200;
+      cameraRef.current = camera; // Store camera reference
+      // Creating a scene
+      scene = new THREE.Scene();
+      // Loading a cube texture as background
+      // Setting background color to blue
+      scene.background = new THREE.Color(0x011c47); // 0x0000ff represents blue color in hexadecimal
+      // Creating a directional light
+      directionalLight = new THREE.DirectionalLight(0xffffff, 6);
+      directionalLight.position.set(1, 1, 1).normalize();
+      scene.add(directionalLight);
+      const pointLight = new THREE.PointLight(0xffffff, 2);
+      pointLight.position.set(1, 5, 0);
+      scene.add(pointLight);
+      // Creating a sphere geometry
+      const geometry = new THREE.SphereGeometry(150);
+      // Creating a basic material with red color
+      const material = new THREE.MeshStandardMaterial({
+        color: new THREE.Color(0x5391f5),
+        roughness: 1,
+        metalness: 1,
+        opacity: 1,
+        emissive: new THREE.Color(0x5391f5),
+        fog: true,
+      });
+      // Creating and adding spheres to the scene
+      for (let i = 0; i < 200; i++) {
+        const mesh = new THREE.Mesh(geometry, material);
+        mesh.position.x = Math.random() * 10000 - 5000;
+        mesh.position.y = Math.random() * 10000 - 5000;
+        mesh.position.z = Math.random() * 10000 - 5000;
+        mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 2 + 1;
+        scene.add(mesh);
+        spheres.current.push(mesh);
+      }
+      // Creating a WebGL renderer
+      renderer = new THREE.WebGLRenderer();
+      renderer.setPixelRatio(window.devicePixelRatio);
+      renderer.setSize(window.innerWidth, window.innerHeight); // Set renderer size to window size
+      container.appendChild(renderer.domElement);
 
-    scene.background = new THREE.Color(0x011c47);
+      // Creating a stereo effect
+      effect = new StereoEffect(renderer);
+      effect.setSize(window.innerWidth, window.innerHeight);
 
-    const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current });
-    renderer.setSize(window.innerWidth, window.innerHeight);
+      // Event listeners for resizing window and mouse movement
+      window.addEventListener("resize", onWindowResize);
+      document.addEventListener("mousemove", onDocumentMouseMove);
+    };
 
-    const material = new THREE.MeshStandardMaterial({
-      color: new THREE.Color(0x5391f5),
-      roughness: 1,
-      metalness: 1,
-      opacity: 1,
-      emissive: new THREE.Color(0x5391f5),
-      fog: true,
-    });
-    const geometry = new THREE.SphereGeometry(150);
-
-    for (let i = 0; i < 500; i++) {
-      const mesh = new THREE.Mesh(geometry, material);
-      mesh.position.x = Math.random() * 10000 - 5000;
-      mesh.position.y = Math.random() * 10000 - 5000;
-      mesh.position.z = Math.random() * 10000 - 5000;
-      mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 2 + 1;
-      scene.add(mesh);
-      spheres.current.push(mesh);
-    }
-
-    const light = new THREE.DirectionalLight(0xffffff, 6);
-    light.position.set(1, 1, 1).normalize();
-    scene.add(light);
-
-    camera.current = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    );
-    camera.current.position.z = 5;
+    const onWindowResize = () => {
+      // Adjusting camera aspect ratio and effect size on window resize
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      effect.setSize(window.innerWidth, window.innerHeight);
+    };
+    const onDocumentMouseMove = (event) => {
+      // Updating mouse position on mouse movement
+      mouseX.current = (event.clientX - window.innerWidth / 2) * 10;
+      mouseY.current = (event.clientY - window.innerHeight / 2) * 10;
+    };
 
     const animate = () => {
+      // Recursive animation function
       requestAnimationFrame(animate);
-      updateCamera();
       render();
     };
 
-    const updateCamera = () => {
-      if (orientationData) {
-        const { alpha, beta, gamma } = orientationData;
-
-        // Convert degrees to radians for Three.js rotation
-        const betaRad = THREE.MathUtils.degToRad(beta);
-        const gammaRad = THREE.MathUtils.degToRad(gamma);
-        const alphaRad = THREE.MathUtils.degToRad(alpha);
-
-        // Set camera rotation based on device orientation
-        camera.current.rotation.set(betaRad, gammaRad, -alphaRad);
-      }
-    };
-
     const render = () => {
+      // Rendering function
       const timer = 0.00001 * Date.now();
-
-      camera.current.lookAt(scene.position);
-
+      // Updating camera position based on mouse movement
+      camera.position.x += (mouseX.current - camera.position.x) * 0.05;
+      camera.position.y += (-mouseY.current - camera.position.y) * 0.05;
+      camera.lookAt(scene.position);
+      // Moving spheres in a circular pattern
       for (let i = 0, il = spheres.current.length; i < il; i++) {
         const sphere = spheres.current[i];
-        sphere.position.x = 1000 * Math.cos(timer + i);
-        sphere.position.y = 1000 * Math.sin(timer + i * 1.1);
+        sphere.position.x = 5000 * Math.cos(timer + i);
+        sphere.position.y = 5000 * Math.sin(timer + i * 1.1);
       }
-
-      renderer.render(scene, camera.current);
+      // Rendering scene with stereo effect
+      effect.render(scene, camera);
     };
-
+    // Initializing the scene and starting animation
+    init();
     animate();
-
+    // Clean up function
     return () => {
-      spheres.current = [];
+      // Removing event listeners and renderer's DOM element on unmount
+      window.removeEventListener("resize", onWindowResize);
+      document.removeEventListener("mousemove", onDocumentMouseMove);
+      containerRef.current.removeChild(renderer.domElement);
     };
-  }, [orientationData]);
-
-  return <canvas ref={canvasRef} />;
+  }, []); // Empty dependency array ensures useEffect runs only once
+  // Returning a div container for the scene
+  return (
+    <div
+      ref={containerRef}
+      id="info"
+      style={{ overflow: "hidden", width: "100%", height: "100%", margin: 0 }}
+    ></div>
+  );
 };
 
 export default SphereScene;
