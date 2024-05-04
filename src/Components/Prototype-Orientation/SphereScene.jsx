8f96fe24925e4f7f9feb37957cdbfae72222b5e3
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-05-04 13:18:15 +0200

    Removing camera mousmovement

--- src/Components/Prototype-Orientation/SphereScene.jsx
+++ src/Components/Prototype-Orientation/SphereScene.jsx
@@ -4,11 +4,8 @@ import { StereoEffect } from "three/examples/jsm/effects/StereoEffect.js";
 
 const SphereScene = ({ orientationData }) => {
   const containerRef = useRef(null);
-  const mouseX = useRef(0);
-  const mouseY = useRef(0);
   const spheres = useRef([]);
   const cameraRef = useRef(null);
-  
   const targetPosition = useRef(new THREE.Vector3());
   const velocity = useRef(new THREE.Vector3());
 
@@ -17,7 +14,6 @@ const SphereScene = ({ orientationData }) => {
 
     const init = () => {
       const container = containerRef.current;
-      // Creating a perspective camera
       camera = new THREE.PerspectiveCamera(
         60,
         window.innerWidth / window.innerHeight,
@@ -25,22 +21,16 @@ const SphereScene = ({ orientationData }) => {
         100000
       );
       camera.position.z = 3200;
-      cameraRef.current = camera; // Store camera reference
-      // Creating a scene
+      cameraRef.current = camera;
       scene = new THREE.Scene();
-      // Loading a cube texture as background
-      // Setting background color to blue
-      scene.background = new THREE.Color(0x011c47); // 0x0000ff represents blue color in hexadecimal
-      // Creating a directional light
+      scene.background = new THREE.Color(0x011c47);
       directionalLight = new THREE.DirectionalLight(0xffffff, 6);
       directionalLight.position.set(1, 1, 1).normalize();
       scene.add(directionalLight);
       const pointLight = new THREE.PointLight(0xffffff, 2);
       pointLight.position.set(1, 5, 0);
       scene.add(pointLight);
-      // Creating a sphere geometry
       const geometry = new THREE.SphereGeometry(150);
-      // Creating a basic material with red color
       const material = new THREE.MeshStandardMaterial({
         color: new THREE.Color(0x5391f5),
         roughness: 1,
@@ -49,7 +39,6 @@ const SphereScene = ({ orientationData }) => {
         emissive: new THREE.Color(0x5391f5),
         fog: true,
       });
-      // Creating and adding spheres to the scene
       for (let i = 0; i < 200; i++) {
         const mesh = new THREE.Mesh(geometry, material);
         mesh.position.x = Math.random() * 10000 - 5000;
@@ -59,90 +48,60 @@ const SphereScene = ({ orientationData }) => {
         scene.add(mesh);
         spheres.current.push(mesh);
       }
-      // Creating a WebGL renderer
       renderer = new THREE.WebGLRenderer();
       renderer.setPixelRatio(window.devicePixelRatio);
-      renderer.setSize(window.innerWidth, window.innerHeight); // Set renderer size to window size
+      renderer.setSize(window.innerWidth, window.innerHeight);
       container.appendChild(renderer.domElement);
-
-      // Creating a stereo effect
       effect = new StereoEffect(renderer);
       effect.setSize(window.innerWidth, window.innerHeight);
-
-      // Event listeners for resizing window and mouse movement
       window.addEventListener("resize", onWindowResize);
-      document.addEventListener("mousemove", onDocumentMouseMove);
     };
 
     const onWindowResize = () => {
-      // Adjusting camera aspect ratio and effect size on window resize
       camera.aspect = window.innerWidth / window.innerHeight;
       camera.updateProjectionMatrix();
       renderer.setSize(window.innerWidth, window.innerHeight);
       effect.setSize(window.innerWidth, window.innerHeight);
     };
-    const onDocumentMouseMove = (event) => {
-      // Updating mouse position on mouse movement
-      mouseX.current = (event.clientX - window.innerWidth / 2) * 10;
-      mouseY.current = (event.clientY - window.innerHeight / 2) * 10;
-    };
 
     const animate = () => {
-      // Recursive animation function
       requestAnimationFrame(animate);
       update();
       render();
     };
 
     const update = () => {
-      // Interpolate camera position towards the target position
       if (orientationData) {
         const { alpha, beta, gamma } = orientationData;
-        targetPosition.current.set(alpha, beta, gamma); // Update target position
-        targetPosition.current.multiplyScalar(0.1); // Adjust scaling for smoothness
-        velocity.current.lerp(targetPosition.current, 0.05); // Adjust lerping speed for smoothness
+        targetPosition.current.set(alpha, beta, gamma);
+        targetPosition.current.multiplyScalar(0.1);
+        velocity.current.lerp(targetPosition.current, 0.05);
         cameraRef.current.position.x = velocity.current.x;
         cameraRef.current.position.y = velocity.current.y;
         cameraRef.current.position.z = velocity.current.z;
       }
     };
+
     const render = () => {
-      // Rendering function
       const timer = 0.00001 * Date.now();
-      // Updating camera position based on mouse movement
-      camera.position.x += (mouseX.current - camera.position.x) * 0.05;
-      camera.position.y += (-mouseY.current - camera.position.y) * 0.05;
-      camera.lookAt(scene.position);
-      // Moving spheres in a circular pattern
+      cameraRef.current.lookAt(scene.position);
       for (let i = 0, il = spheres.current.length; i < il; i++) {
         const sphere = spheres.current[i];
         sphere.position.x = 5000 * Math.cos(timer + i);
         sphere.position.y = 5000 * Math.sin(timer + i * 1.1);
       }
-
-      if (orientationData) {
-        const { alpha, beta, gamma } = orientationData;
-        // You need to adjust camera position based on alpha, beta, gamma values
-        camera.position.x = alpha;
-        camera.position.y = beta;
-        camera.position.z = gamma;
-      }
-
-      // Rendering scene with stereo effect
-      effect.render(scene, camera);
+      effect.render(scene, cameraRef.current);
     };
-    // Initializing the scene and starting animation
+
     init();
     animate();
-    // Clean up function
+
     return () => {
-      // Removing event listeners and renderer's DOM element on unmount
       window.removeEventListener("resize", onWindowResize);
-      document.removeEventListener("mousemove", onDocumentMouseMove);
       containerRef.current.removeChild(renderer.domElement);
     };
-  }, [orientationData]); // Empty dependency array ensures useEffect runs only once
-  // Returning a div container for the scene
+  }, [orientationData]);
+
   return (
     <div
       ref={containerRef}
