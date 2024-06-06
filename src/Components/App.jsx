62fff9c015f8f3a05b839b68ad63122d5194d1b4
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-06-06 12:52:24 +0200

    Testing on phone

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import SphereScene from "../Projec WorkZen/Visuals/SphereSceneVisual";
 import SnowingScene from "../Projec WorkZen/Visuals/SnowingSceneVisual";
 import NorthernLightsScene from "../Projec WorkZen/Visuals/NorthernLightScene";
+import BreathingCircle from "../Projec WorkZen/Visuals/BreathingCircle";
+import GradientChange from "../Projec WorkZen/Visuals/GradientChange";
 
 function App() {
   return (
     <div className="App">
       {/* <SnowingScene /> */}
-      <SphereScene />
-      {/* <NorthernLightsScene /> */}
+      {/* <SphereScene /> */}
+      <BreathingCircle />
     </div>
   );
 }
